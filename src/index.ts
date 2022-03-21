9f16a9157be0c1b831a5e420cd0eac150272add2
GoogleFeud <volen.sl666@gmail.com>
2022-03-21 22:14:53 +0200

    Implement visitor

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,7 @@ export default (program: ts.Program): ts.TransformerFactory<ts.Node> => () => {
         return transformer.run(firstNode as ts.SourceFile);
     };
 };
+
+export type Assert<T> = T | T & { __marker: "assert" };
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export type OrReturn<T, _ReturnValue> = T | T & { __marker: "or-return" }; 
--- src/transformer.ts
+++ src/transformer.ts
@@ -1,6 +1,5 @@
 import ts from "typescript";
 
-
 export class Transformer {
     checker: ts.TypeChecker;
     constructor(program: ts.Program) {
@@ -10,6 +9,32 @@ export class Transformer {
     run(node: ts.SourceFile) : ts.SourceFile {
         //if (node.isDeclarationFile) return node;
         return node;
+    }
+
+    visitor(node: ts.Node, body: Array<ts.Statement> = []) : ts.VisitResult<ts.Node> {
+
+        if (ts.isFunctionDeclaration(node) || ts.isArrowFunction(node)) {
+            if (!node.body) return node;
+            const fnBody: Array<ts.Statement> = [];
+            // TBD: Check if any of the markers are in the function parameters, and if so only do the following AND run the markers
+            if (ts.isBlock(node.body)) {
+                for (const exp of node.body.statements) {
+                    this.visitor(exp, fnBody);
+                }
+            } else {
+                this.visitor(node.body, fnBody);
+                if (fnBody.length) {
+                    const last = fnBody[fnBody.length - 1] as ts.Statement; 
+                    if (ts.isExpressionStatement(last)) fnBody[fnBody.length - 1] = ts.factory.createReturnStatement(last.expression);
+                }
+            }
+            return ts.factory.createFunctionDeclaration(node.decorators, node.modifiers, node.asteriskToken, node.name, node.typeParameters, node.parameters, node.type, ts.factory.createBlock(fnBody));
+        } 
+        else if (ts.isAsExpression(node)) {
+            // TBD: Check if the asserted value is a marker, if it is run it...
+            return node;
+        } else 
+            return ts.forEachChild(node, (node) => this.visitor(node, body));
     }
 
 }
