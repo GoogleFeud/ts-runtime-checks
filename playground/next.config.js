b9ff9a1b5c1a44809ae4af22e781285ce12d2a8e
GoogleFeud <volen.sl666@gmail.com>
2022-03-29 14:35:03 +0300

    Ignore eslint errors

--- playground/next.config.js
+++ playground/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: "./"
+    assetPrefix: "./",
+    eslint: {
+        ignoreDuringBuilds: true
+    },
 };
 
 module.exports = nextConfig;
