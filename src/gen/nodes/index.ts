89644ff1acfe02a0eab4771543c120bc574932cc
GoogleFeud <volen.sl666@gmail.com>
2023-06-08 20:10:49 +0300

    Better message system

--- src/gen/nodes/index.ts
+++ src/gen/nodes/index.ts
@@ -1,7 +1,7 @@
 import ts from "typescript";
-import { TypeDataKinds, Validator } from "../validators";
-import { MESSAGES } from "./messages";
-import { _bin, _if, _new, _num, _throw } from "./utils";
+import { NumberTypes, TypeDataKinds, Validator } from "../validators";
+import { MESSAGES, concat, joinElements } from "./messages";
+import { _bin, _bin_chain, _new, _num, _throw, _typeof_cmp } from "./utils";
 
 export interface ValidationResultType {
     throw?: boolean,
@@ -15,22 +15,61 @@ export interface NodeGenContext {
     resultType: ValidationResultType
 }
 
-export function error(ctx: NodeGenContext, msg: keyof typeof MESSAGES, ...params: Array<Validator|string>) : ts.Statement {
+export interface GenResult {
+    condition: ts.Expression,
+    error: ts.Expression,
+    extra?: ts.Statement[]
+}
+
+export function emptyGenResult() : GenResult {
+    return {
+        condition: ts.factory.createNull(),
+        error: ts.factory.createNull()
+    };
+}
+
+export function error(ctx: NodeGenContext, errorMsg: ts.Expression) : ts.Statement {
     if (ctx.resultType.return) return ts.factory.createReturnStatement(ctx.resultType.return);
-    //@ts-expect-error Expected
-    const errorMsg = ts.factory.createStringLiteral(MESSAGES[msg](...params.map(p => p.toString())));
     if (ctx.resultType.returnErr) return ts.factory.createReturnStatement(errorMsg);
     else if (ctx.resultType.custom) return ctx.resultType.custom(errorMsg);
     else return _throw(_new(ctx.errorTypeName, [errorMsg]));
 }
 
-export function genNode(validator: Validator, ctx: NodeGenContext) : ts.Statement[] {
+export function genNode(validator: Validator) : GenResult {
     switch (validator.typeData.kind) {
     case TypeDataKinds.Number: {
-        if (validator.typeData.literal) return _if(_bin(validator.expression, _num(validator.typeData.literal), ts.SyntaxKind.EqualsEqualsEqualsToken), )
-        return [];
+        if (validator.typeData.literal) return {
+            condition: _bin(validator.expression, _num(validator.typeData.literal), ts.SyntaxKind.EqualsEqualsEqualsToken),
+            error: MESSAGES.ToBeEqual(validator.path(), validator.typeData.literal.toString())
+        };
+        const errorMessages = [];
+        const checks: ts.Expression[] = [_typeof_cmp(validator.expression, "number")];
+        if (validator.typeData.type === NumberTypes.Integer) {
+            checks.push(_bin(_bin(validator.expression, _num(1), ts.SyntaxKind.PercentToken), _num(0), ts.SyntaxKind.ExclamationEqualsEqualsToken));
+            errorMessages.push("to be an integer");
+        }
+        else if (validator.typeData.type === NumberTypes.Float) {
+            checks.push(_bin(_bin(validator.expression, _num(1), ts.SyntaxKind.PercentToken), _num(0), ts.SyntaxKind.EqualsEqualsEqualsToken));
+            errorMessages.push(" to be a float");
+        } else {
+            errorMessages.push("to be a number");
+        }
+
+        if (validator.typeData.min !== undefined) {
+            checks.push(_bin(validator.expression, validator.typeData.min, ts.SyntaxKind.GreaterThanToken));
+            errorMessages.push(concat`to be greater than ${validator.typeData.min}`);
+        }
+
+        if (validator.typeData.max !== undefined) {
+            checks.push(_bin(validator.expression, validator.typeData.max, ts.SyntaxKind.LessThanToken));
+            errorMessages.push(concat`to be less than ${validator.typeData.max}`);
+        }
+
+        return {
+            condition: _bin_chain(checks, ts.SyntaxKind.AmpersandAmpersandToken),
+            error: joinElements(errorMessages)
+        }
     }
-    default: return [];
+    default: return emptyGenResult();
     }
 }
-
--- src/gen/nodes/messages.ts
+++ src/gen/nodes/messages.ts
@@ -1,8 +1,36 @@
+import ts from "typescript";
+import { _bin_chain, _str } from "./utils";
 
+export type Stringifyable = string | ts.Expression;
 
 export const MESSAGES = {
-    MustBeEqual: (a: string, b: string) => `${a} must be equal to ${b}.`,
-    ToBe: (a: string, b: string) => `Expected ${a} to be ${b}.`,
-    MinLen: (a: string, b: string) => `Expected ${a} to have a minimum length of ${b}.`,
-    MaxLen: (a: string, b: string) => `Expected ${a} to have a minimum length of ${b}.`
-};
+    ToBeEqual: (a: Stringifyable, b: Stringifyable) => concat`${a} to be equal to ${b}.`,
+    ToBe: (a: Stringifyable, b: Stringifyable) => concat`${a} to be ${b}.`,
+    MinLen: (a: Stringifyable, b: Stringifyable) => concat`${a} to have a minimum length of ${b}.`,
+    MaxLen: (a: Stringifyable, b: Stringifyable) => concat`${a} to have a minimum length of ${b}.`
+} as const;
+
+export function concat(strings: TemplateStringsArray, ...elements: Stringifyable[]) : ts.Expression {
+    const finalElements: Stringifyable[] = [];
+    for (let i=0; i < strings.length; i++) {
+        finalElements.push(strings[i] as string);
+        if (i < elements.length) finalElements.push(elements[i] as Stringifyable);
+    }
+    return joinElements(finalElements);
+}
+
+export function joinElements(elements: Stringifyable[]) : ts.Expression {
+    const output: ts.Expression[] = [];
+    for (const element of elements) {
+        const lastElement = output[output.length - 1];
+        if (lastElement && (ts.isStringLiteral(lastElement) || ts.isNumericLiteral(lastElement))) {
+            if (typeof element === "string") output[output.length - 1] = _str(lastElement.text + element);
+            else if (ts.isStringLiteral(element) || ts.isNumericLiteral(element)) output[output.length - 1] = _str(lastElement.text + element.text);
+            else output.push(element);
+        } else {
+            if (typeof element === "string") output.push(_str(element));
+            else output.push(element);
+        }
+    }
+    return _bin_chain(output, ts.SyntaxKind.PlusToken);
+}
