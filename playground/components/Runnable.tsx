7985cf8a7b555b8f95ba56a42b6b6c939f8ed441
GoogleFeud <volen.sl666@gmail.com>
2023-07-05 16:17:13 +0300

    Update playground

--- playground/components/Runnable.tsx
+++ playground/components/Runnable.tsx
@@ -1,13 +1,15 @@
 import SplitPane from "react-split-pane";
-import { Highlight } from "./Highlight";
 import styles from "../css/App.module.css";
 import { useState } from "react";
+import Editor from "@monaco-editor/react";
 
 export function Runnable(props: { code: string }) {
     const [evalRes, setEvalRes] = useState<string>();
-    return <SplitPane split="horizontal" defaultSize={"70%"} primary="first">
-        <div>
-            <Highlight text={props.code} />
+    return <SplitPane split="horizontal" defaultSize={"75%"} primary="first">
+        <div style={{width: "100%"}}>
+            <Editor height={"80vh"} language="javascript" theme="vs-dark" value={props.code} onMount={editor => {
+                editor.updateOptions({ readOnly: true });
+            }} />;
         </div>
         <div className={styles.runSection}>
             <button className={styles.button} onClick={() => {
