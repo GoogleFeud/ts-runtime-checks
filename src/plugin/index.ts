2137b2b86f10b88efaece5878556d03ea84f9e61
GoogleFeud <volen.sl666@gmail.com>
2024-08-04 16:06:44 +0300

    Experiment with ways to transform

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,6 +1,6 @@
 import {TsRuntimeChecksConfig} from "../index";
 import {Transformer} from "../transformer";
-import ts from "typescript";
+import ts, {TransformationContext} from "typescript";
 
 export type ProgramType = ts.SemanticDiagnosticsBuilderProgram;
 export type WatcherHost = ts.WatchCompilerHostOfFilesAndCompilerOptions<ProgramType>;
@@ -18,15 +18,44 @@ export class PluginContext {
      * file B's type, and file A's type updates,
      */
     //private relationsBetweenFiles: Map<string, string[]>;
+    private tsconfig: ts.ParsedCommandLine;
     private previousTransformer?: Transformer;
+    private transformers: ts.TransformerFactory<ts.SourceFile | ts.Bundle>[];
     constructor(
-        public config: ConfigInfo,
+        config: ConfigInfo,
         public tsrsConfig: TsRuntimeChecksConfig,
         public watcher: WatcherProgram
-    ) {}
+    ) {
+        this.tsconfig = config[1];
+        this.tsconfig.options.sourceMap = true;
+        this.tsconfig.options.inlineSources = true;
+        const transformerFactory: (ctx: TransformationContext) => (file: ts.SourceFile) => (ts.SourceFile) = ctx => {
+            const program = this.watcher.getProgram().getProgram();
+            let transformer: Transformer;
+            if (this.previousTransformer) transformer = this.previousTransformer.extend(program, ctx);
+            else transformer = new Transformer(program, ctx, this.tsrsConfig);
+            this.previousTransformer = transformer;
+            return file => transformer.run(file);
+        };
+        //this.transformers = [transformerFactory, ts.transformESNext as unknown as (ctx: TransformationContext) => (file: ts.SourceFile) => ts.SourceFile];
+        this.transformers = [
+            ...ts.getTransformers(
+                this.tsconfig.options,
+                {
+                    before: [transformerFactory]
+                },
+                ts.EmitOnly.Js
+            ).scriptTransformers
+        ];
+        console.log(this.transformers);
+    }
 
     finish() {
         this.watcher.close();
+    }
+
+    shouldTransform(fileName: string): boolean {
+        return this.tsconfig.fileNames.includes(fileName);
     }
 
     /**
@@ -35,25 +64,14 @@ export class PluginContext {
      *
      * _emitFile: EmitFileFn
      */
-    transform(fileName: string, currentContent: string, emitJs?: boolean): string {
+    transform(fileName: string): string | undefined {
         const program = this.watcher.getProgram();
         const sourceFile = program.getSourceFile(fileName);
-        if (!sourceFile) return currentContent;
-        const currentProgram = program.getProgram();
-        const transContext = { factory: ts.factory } as ts.TransformationContext;
-        let transformer;
-        if (this.previousTransformer) transformer = this.previousTransformer.extend(currentProgram, transContext);
-        else transformer = new Transformer(currentProgram, transContext, this.tsrsConfig);
-        this.previousTransformer = transformer;
-
-        const transformedSource = transformer.run(sourceFile);
-
-        if (emitJs) {
-            let output = currentContent;
-            program.emit(transformedSource, (_, content) => (output = content));
-            return output;
-        }
-
+        if (!sourceFile) return;
+        const transformResult = ts.transform(sourceFile, this.transformers, this.tsconfig.options);
+        const transformedSource = transformResult.transformed[0];
+        if (!transformedSource || ts.isBundle(transformedSource)) return;
+        // TODO: Do something with diagnostics?
         return PluginContext.printer.printFile(transformedSource);
     }
 
@@ -74,7 +92,17 @@ export class PluginContext {
     }
 
     static createWatchHost(config: ts.ParsedCommandLine): WatcherHost {
-        const host = ts.createWatchCompilerHost(config.fileNames, config.options, ts.sys, ts.createSemanticDiagnosticsBuilderProgram, this.diagnosticReporter);
+        const system = {...ts.sys};
+        system.write = _ => {
+            _;
+        };
+        system.clearScreen = () => {
+            void 0;
+        };
+        system.writeFile = _ => {
+            _;
+        };
+        const host = ts.createWatchCompilerHost(config.fileNames, config.options, system, ts.createSemanticDiagnosticsBuilderProgram, this.diagnosticReporter);
         return host;
     }
 
--- src/unplugin/factory.ts
+++ src/unplugin/factory.ts
@@ -1,24 +1,27 @@
 import {type UnpluginFactory} from "unplugin";
 import {TsRuntimeChecksConfig} from "../index";
-import {PluginContext} from "./core";
+import {PluginContext} from "../plugin";
+
+let ctx: PluginContext;
 
 export const unpluginFactory: UnpluginFactory<TsRuntimeChecksConfig | undefined> = (options = {}) => {
-    const ctx = PluginContext.createContext(options);
-
-    if (!ctx) return {name: "unplugin-ts-runtime-checks"};
-
     return {
         name: "unplugin-ts-runtime-checks",
+        buildStart() {
+            if (ctx) return;
+            ctx = PluginContext.createContext(options)!;
+        },
         transformInclude(fileName) {
             // TODO: Allow more
             return fileName.endsWith(".ts") || fileName.endsWith(".tsx");
         },
         transform(code, fileName) {
-            console.log("TRANSFORMING: ", fileName);
-            return ctx.transform(fileName, code);
+            const transformed = ctx.transform(fileName);
+            console.log("TRANSFORMED: ", fileName, transformed);
+            return transformed || code;
         },
         buildEnd() {
             ctx.finish();
-        },
+        }
     };
 };
